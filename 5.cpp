5faf48dc2860b6e8f97c2eb0f31d89139267da54
mohammadreza aldaghi <mohammadreza12345600@gmail.com>
2023-05-17 11:39:39 +0330

    fddf

--- 5.cpp
+++ 5.cpp
@@ -3,8 +3,11 @@ int main()
 {
     int arr[] = { 10, 20, 30, 40, 50, 60 };
     int *ptr1 = arr;
+    // printf("%d\n",*ptr1);
     int *ptr2 = arr + 5;
     printf("%d\n", (*ptr2 - *ptr1));
     printf("%c", (char)(*ptr2 - *ptr1));
+    // Output = 50
+    //Output = 2
     return 0;
 }
