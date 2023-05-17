f928b2bf25144e277f00bf7c0f12e6922d4d8d43
sana <zsfattahi400@gmail.com>
2023-05-17 10:53:14 +0330

    fixed:)

--- 1.cpp
+++ 1.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 class container {
-
+protected:
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container(){}
+	container(int s) :size(s) {}
+    int& getsize() { return size; }
+	friend class vector;
 
 };
 
@@ -15,14 +17,20 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l), container(1 * 100) {
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen()  {
+		call_num++;
 		return len;
 	}
+	vector(const container& c) :container(1 * 100) {
+		this->size = c.size;
+		this->p = new float[c.size];
+		len = 0;
+	}
+
 	~vector() = default;
 };
 
@@ -34,7 +42,7 @@ int main() {
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2 = (vector)100;
+	v2.getlen() = 40;
 	cout << v2.getlen();
 }
