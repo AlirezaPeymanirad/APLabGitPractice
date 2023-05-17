3fcf8c9915140e87feba27daa6c0eac46b9347b1
Mahrokh <mahrokhmousavii44@gmail.com>
2023-05-17 10:56:06 +0330

    Bugs fixed

--- 3.cpp
+++ 3.cpp
@@ -1,9 +1,10 @@
-﻿#include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#pragma warning(disable:4996)
 #define MAX_SIZE 200
 int arr[MAX_SIZE];
 
-typedef struct alfa * alfaptr;
+typedef struct alfa* alfaptr;
 
 struct alfa {
 	long long x;
@@ -45,7 +46,7 @@ void search(int x)
 			printf("ERROR2");
 			break;
 		}
-		node = node->next;
+	node = node->next;
 }
 
 void rpop() {//pop last element
@@ -66,9 +67,9 @@ void set()
 int size()
 {
 	alfaptr node = front;
-	int count;
+	int count=0;
 	while (node)
-		count++;node = node->next;
+		count++; node = node->next;
 	return count;
 }
 
@@ -88,7 +89,7 @@ int average()
 {
 
 	alfaptr node = front;
-	int sum = 0, count;
+	int sum = 0, count=0;
 	while (node) {
 		sum += node->x;
 		count++;
