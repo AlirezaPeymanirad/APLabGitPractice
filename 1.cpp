c980ff62f57ad655e17eff999925f269a18446f6
rasoul6094 <123249130+rasoul6094@users.noreply.github.com>
2023-05-17 09:57:38 +0330

    1.cpp fixed by rasoul(40125933)

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,64 @@
 #include <iostream>
 using namespace std;
 
-class container {
+class container
+{
 
-	int size;
 public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container() { size = 0; }
+	float *p;
+	container(int s) : size(s) {}
+	int getsize() const { return size; }
+	int &getsize() { return size; }
 
+protected:
+	int size;
 };
 
-class vector :public container {
+class vector : public container
+{
+private:
+int len;
 
-	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
+	static int call_num;
+
+	explicit vector(int l) : len(l), container(1 * 100)
+	{
+		p = new float[size];
 	}
-	int len;
-	int& getlen() const {
-		call_num ++;
+	vector(const container &con) : container(1 * 100)
+	{
+
+		size = con.getsize();
+		p = new float[size];
+		len = 0;
+	}
+	int getlen() const
+	{
+		call_num++;
+		return len;
+	}
+	int &getlen()
+	{
+		call_num++;
 		return len;
 	}
 	~vector() = default;
 };
+int vector::call_num = 0;
 
-int main() {
+int main()
+{
 
 	container c1(100);
 	vector v1 = c1;
-	container& r1 = v1;
+	container &r1 = v1;
 	container c2 = 100;
 	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
+	return 0;
 }
