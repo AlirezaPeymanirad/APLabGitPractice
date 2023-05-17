a123c180631d32b4243dbc0f06fe8e03b7c08c3b
Saba Rafiei <saba.ra1382@gmail.com>
2023-05-17 11:53:13 +0430

    All bugs are fixed now

--- 5.cpp
+++ 5.cpp
@@ -4,7 +4,7 @@ int main()
     int arr[] = { 10, 20, 30, 40, 50, 60 };
     int *ptr1 = arr;
     int *ptr2 = arr + 5;
-    printf("%d\n", (*ptr2 - *ptr1));
-    printf("%c", (char)(*ptr2 - *ptr1));
+    printf("%d\n", (*ptr2 - *ptr1)); //Result: 50
+    printf("%c", (char)(*ptr2 - *ptr1)); //Result: 2
     return 0;
 }
--- 6.cpp
+++ 6.cpp
@@ -6,6 +6,6 @@ int main()
 	x = (char *)&a;
 	a = 512;
 	x[0] = 1;
-	printf("%d\n", a);
+	printf("%d\n", a); //513
 	return 0;
 }
