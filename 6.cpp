c51cdc323e5d20ae78ecd777a32c56faaa8c1fba
seyedali <amirisayedali@gmail.com>
2023-05-18 16:18:25 +0430

    check 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -2,10 +2,12 @@
 int main()
 {
 	int a;
-	char *x;
-	x = (char *)&a;
+	char* x;
+	x = (char*)&a;
 	a = 512;
 	x[0] = 1;
 	printf("%d\n", a);
 	return 0;
 }
+//00000001,00000001  are two bytes of a
+//output:513
