efad0876b62cd1520175985fb52508a294aa72c9
AmiiirHg <hajigol6606@gmail.com>
2023-05-19 00:02:52 +0330

    change

--- 8.cpp
+++ 8.cpp
@@ -7,7 +7,5 @@ int main() {
 	const char * str[] = { "Wish","You","Best",":D" };
 	printf("%c%c ", *f(str), *(f(str) + 1));
 	printf("%c%c%c%c\n", **str, *(*(str + 1) + 1), *((str + 2)[-1] + 1), **&*(&str[-1] + 1));
-
-
-	
 }
+//Be WooW
