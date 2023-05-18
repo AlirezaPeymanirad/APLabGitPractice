bc0ec352ecb2432c16efdca382accd15ae6c446d
seyedali <amirisayedali@gmail.com>
2023-05-18 16:21:35 +0430

    check 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -2,9 +2,11 @@
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5 };
-	int *p = arr;
-	++*p;
+	int* p = arr;
+	++* p;
+	//taghadom amalgar * az ++ bishtar ast yani ++(*P) ejra mishavad
 	p += 2;
 	printf("%d", *p);
 	return 0;
 }
+//output:3
