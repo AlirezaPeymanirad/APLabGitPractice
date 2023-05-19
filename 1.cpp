d6de76bfa03a838ffb7da2c55917d9aa19572336
faezehghiasi <f.ghiasi@ec.iut.ac.ir>
2023-05-19 13:14:13 +0330

    file debug shod va taghirat anjam shod

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 class container {
@@ -6,8 +6,8 @@ class container {
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container(int s) :size(s) {}
+	int& getsize() { return size; }
 
 };
 
@@ -15,26 +15,31 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l), container(1 * 100) {
 		p = new float();
 	}
-	int len;
-	int& getlen() const {
-		call_num ++;
+	 int len;
+	 const int& getlen() const {
+		//call_num++; // تابع کانست نمیتواد داده های شی را تغییر دهد
 		return len;
-	}
+	 }
+	 int& getlen()  {
+		 call_num++;
+		 return len;
+	 }
 	~vector() = default;
 };
 
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
+	//vector v1 = c1; // کلاس فرزند نمیتواند برابر کلاس پدر قرار گیرد
+	vector v1(100);
 	container& r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
-	cout << v2.getlen();
+	c2.getsize() = 20; 
+	cout << c2.getsize()<<endl;
+	vector v2 = (vector)100;
+	v2.getlen() = 40;
+	cout << v2.getlen()<<endl;
 }
